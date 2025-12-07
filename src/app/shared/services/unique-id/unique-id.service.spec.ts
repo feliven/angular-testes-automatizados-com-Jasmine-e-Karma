148137dcff554398932bf9d1c86460fee9b10a54
FVS-MBA <feliven@gmail.com>
2025-12-07 14:59:15 -0300

    cria teste para UniqueIdService sobre gerarIDComPrefixo dar erro com prefixo vazio

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -44,4 +44,23 @@ describe(UniqueIdService.name, () => {
 
     expect(numeroIDs).toEqual(totalTentativas);
   });
+
+  it(`-${UniqueIdService.prototype.gerarIDComPrefixo.name}- deve dar erro quando for chamado com valor vazio`, () => {
+    // quando você está chamando um método e você quer testar se ele lança uma exceção ou não,
+    // você tem que chamar esse método dentro de uma função,
+    // então após o expect eu passei uma arrow function com uma única instrução
+
+    expect(() => service.gerarIDComPrefixo(undefined)).toThrow();
+    expect(() => service.gerarIDComPrefixo(null)).toThrow();
+    expect(() => service.gerarIDComPrefixo('')).toThrow();
+    expect(() => service.gerarIDComPrefixo(NaN as unknown as string)).toThrow();
+  });
+
+  it(`-${UniqueIdService.prototype.gerarIDComPrefixo.name}- deve dar erro quando for chamado com valor vazio de um conjunto`, () => {
+    const valoresVazios = [undefined, null, '', NaN as unknown as string];
+
+    valoresVazios.forEach((valor) => {
+      expect(() => service.gerarIDComPrefixo(valor)).toThrow();
+    });
+  });
 });
